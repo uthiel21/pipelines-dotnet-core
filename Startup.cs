5f8befb678efad0bbe45a3ff607591e0543db4ad
Ulrike Thiel <ulrike.thiel@quibiq.de>
2022-04-04 11:03:44 +0200

    dsf

--- Startup.cs
+++ Startup.cs
@@ -28,6 +28,7 @@ namespace pipelines_dotnet_core
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
+
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
